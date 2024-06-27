52a5b92c38b1f353391722df27e72059a7dbacbd
Diana725 <wachira.wangari2019@students.jkuat.ac.ke>
2024-06-27 14:32:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+function getAgeGroup(yearOfBirth) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - yearOfBirth;
+
+    if (yearOfBirth > 1901 && yearOfBirth <= 1927) {
+        return "Greatest Generation (1901-1927)";
+    } else if (yearOfBirth <= 1945) {
+        return "Silent Generation (1928-1945)";
+    } else if (yearOfBirth <= 1964) {
+        return "Baby Boomers (1946-1964)";
+    } else if (yearOfBirth <= 1980) {
+        return "Generation X (1965-1980)";
+    } else if (yearOfBirth <= 1996) {
+        return "Millennials (1981-1996)";
+    } else if (yearOfBirth <= 2010) {
+        return "Generation Z (1997-2010)";
+    } else if (yearOfBirth <= 2025) {
+        return "Generation Alpha (2011-2024)";
+    } else {
+        return "Future Generation (2025 and beyond)";
+    }
+}
+
+function displayAgeGroup() {
+    const yearOfBirth = document.getElementById('age').value;
+    if (yearOfBirth < 1901) {
+        document.getElementById('result').innerText = 'Please Enter An Year of Birth Greater that 1901.';
+    }
+    else {
+        const result = getAgeGroup(yearOfBirth);
+        document.getElementById('result').innerText = `You belong to: ${result}`;
+    }
+}
